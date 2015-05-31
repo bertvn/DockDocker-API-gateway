3b15087edc18ed038b640b2a137a05cb4663f10a
Bert van Nimwegen <bert_v_n@hotmail.com>
2015-05-31 21:37:04 +0200

    first commit

---
+++ src/main/java/com/mycompany/dockdocker/api/gateway/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dockdocker.api.gateway;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author Bert
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // check documentation @ http://sparkjava.com/documentation.html
+        port(8080);
+        get("/hello",(req,res) -> "Hello World");
+    }
+    
+}
